68e0f213356cc982263c6363e2117a0c85398994
faxbob1129 <fakhriddin1129@gmail.com>
2025-07-24 18:30:14 +0500

    Optimistic looking

--- internal/models/Files.go
+++ internal/models/Files.go
@@ -7,16 +7,17 @@ import (
 
 type Files struct {
 	gorm.Model
-	Year   string `json:"Year" gorm:"type:varchar(255)"`
-	Month  string `json:"Month" gorm:"type:varchar(255)"`
-	Day    string `json:"Day" gorm:"type:varchar(255)"`
-	Ip     string `json:"Ip" gorm:"type:varchar(255)"`
-	Device string `json:"Device"`
-	Url    string `json:"Url" gorm:"type:varchar(255)"`
-	Name   string `json:"Name" gorm:"type:varchar(255)"`
-	Count  int    `json:"Count"`
-	Size   string `json:"Size"`
-	Format string `json:"Format"`
+	Year    string `json:"Year" gorm:"type:varchar(255)"`
+	Month   string `json:"Month" gorm:"type:varchar(255)"`
+	Day     string `json:"Day" gorm:"type:varchar(255)"`
+	Ip      string `json:"Ip" gorm:"type:varchar(255)"`
+	Device  string `json:"Device"`
+	Url     string `json:"Url" gorm:"type:varchar(255)"`
+	Name    string `json:"Name" gorm:"type:varchar(255)"`
+	Count   int    `json:"Count"`
+	Size    string `json:"Size"`
+	Format  string `json:"Format"`
+	Version int    `gorm:"version"`
 }
 
 func (Files) TableName() string {
